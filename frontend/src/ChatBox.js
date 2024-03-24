7b64a302f6943bc9b4ad3840bd685cc6c417a5bb
2581031a <2581031A@student.gla.ac.uk>
2024-03-24 16:03:15 +0000

    /

--- frontend/src/ChatBox.js
+++ frontend/src/ChatBox.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Message from './Message';
 import InputBox from './InputBox';
 import axios from 'axios';
-
+import CryptoJS from 'crypto-js';
 
 
 const ChatBox = ({ activeChat, readOnly }) => {
@@ -16,26 +16,38 @@ const ChatBox = ({ activeChat, readOnly }) => {
         }
     }, [activeChat]);
 
+    const updateGlobalChats = () => {
+        let encryptedChats = localStorage.getItem('chatHistory');
+        let chats = JSON.parse(CryptoJS.AES.decrypt(encryptedChats, process.env.REACT_APP_ENCRYPT_SECRET_KEY).toString(CryptoJS.enc.Utf8)); // Decrypting
+        const chatIndex = chats.findIndex(chat => chat.id === activeChat.id);
+        if (chatIndex !== -1) {
+            chats[chatIndex].messages = [...messages]; // Ensure to update with the latest messages
+            encryptedChats = CryptoJS.AES.encrypt(JSON.stringify(chats), process.env.REACT_APP_ENCRYPT_SECRET_KEY).toString(); // Encrypting again
+            localStorage.setItem('chatHistory', encryptedChats);
+        }
+    };
+
     const sendMessage = async (userMessage) => {
-        if (readOnly || !activeChat || isAITyping) return; // Check if AI is already processing
-        setIsAITyping(true); // AI starts processing
+        if (readOnly || !activeChat || isAITyping) return;
+        setIsAITyping(true);
         const userMessageObj = { text: userMessage, sender: 'user' };
         setMessages([...messages, userMessageObj]);
-        activeChat.messages.push(userMessageObj);
-        
-        
+        activeChat.messages.push(userMessageObj); // Assuming activeChat is a reference type and this updates state
+
         try {
             const response = await axios.post(`${process.env.REACT_APP_API_URL}/ask`, { prompt: userMessage });
             const aiMessageObj = { text: response.data.response.trim(), sender: 'ai' };
             setMessages(messages => [...messages, aiMessageObj]);
-            activeChat.messages.push(aiMessageObj);
+            activeChat.messages.push(aiMessageObj); // Update active chat messages
+            updateGlobalChats(); // Call here after receiving AI response
         } catch (error) {
             console.error("Error sending message:", error);
             const errorMessageObj = { text: "Error getting response.", sender: 'ai' };
             setMessages(messages => [...messages, errorMessageObj]);
-            activeChat.messages.push(errorMessageObj);
+            activeChat.messages.push(errorMessageObj); // Update active chat messages on error too
         } finally {
-            setIsAITyping(false); // AI finishes processing
+            setIsAITyping(false);
+            updateGlobalChats(); // Ensure this is also called here to capture all message updates
         }
     };
 
